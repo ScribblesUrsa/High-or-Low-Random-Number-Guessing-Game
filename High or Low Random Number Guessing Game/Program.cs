043ed72143ebc1840f25cb75cee0d4f3e8802b95
ScribblesUrsa <122501558+ScribblesUrsa@users.noreply.github.com>
2023-08-02 21:58:47 -0600

    Updated Version of the game.
    File uploaded was a scratch file

--- High or Low Random Number Guessing Game/Program.cs
+++ High or Low Random Number Guessing Game/Program.cs
@@ -1,71 +1,84 @@
-﻿using System.Transactions;
-
-namespace High_or_Low_Random_Number_Guessing_Game
+namespace Guessing_Game_Again
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-
-            Console.WriteLine("Welcome to the guessing game!!!");
-
-            //Variables for maximum and minimum range and provide flexibility on the number of guesses allowed
-            const int MIN_VALUE = 0;
-            const int MAX_VALUE = 100;
-            const int NUMBER_OF_GUESSES = 10;
-
-            Random range = new Random();
-            int randomNumber = range.Next(MIN_VALUE, (MAX_VALUE + 1));//(maxValue + 1 to give the true random number range
-            Console.WriteLine(randomNumber);//Just a guideline on whether or not the code works
-
-            Console.WriteLine($"Hello! Guess a whole number that the computer will generate between {MIN_VALUE}  to {MAX_VALUE}:");
-            int userNumber = Convert.ToInt32(Console.ReadLine()); //To avoid error since readline is normally accepting string inputs, the input must be converted to 
-                                                                  //an integer or equivalent         
-            if (randomNumber == userNumber)
             {
-                Console.WriteLine("You guessed it right in one go!");
-            }
+                Console.WriteLine("Welcome to the guessing game!!!");
 
-            if (userNumber > MAX_VALUE)//For the input a number higher than maxValue
-            {
-                Console.WriteLine($"Please Enter a number that is lower than {MAX_VALUE}.");
-                userNumber = Convert.ToInt32(Console.ReadLine());
-            }
+                //Variables for maximum and minimum range and provide flexibility on the number of guesses allowed
+                const int MIN_VALUE = 0;
+                const int MAX_VALUE = 100;
+                const int NUMBER_OF_GUESSES = 10;
 
-            if (userNumber < MIN_VALUE)//For the input a number lower than minValue
-            {
-                Console.WriteLine($"Please Enter a number that is lower than {MIN_VALUE}.");
-                userNumber = Convert.ToInt32(Console.ReadLine());
-            }
+                Random range = new Random();
+                int randomNumber = range.Next(MIN_VALUE, (MAX_VALUE + 1));//(maxValue + 1 to give the true random number range
+                Console.WriteLine(randomNumber);//Just a guideline on whether or not the code works
 
-            for (int i = NUMBER_OF_GUESSES; i >= 1 && (userNumber != randomNumber); i--)//Countdown to number of guesses, it will iterate as long as the guesses remain above 
-                                                                                    //or is equa to one and as long as userNumber is not equal to randomNumber
-            {
-                if (userNumber > randomNumber)//Provides the code for wwhen the user guesses a number that is higher than what the random number actually is
+                Console.WriteLine($"Hello! Guess a whole number that the computer will generate between {MIN_VALUE}  to {MAX_VALUE}:");
+                String userInput = "";
+                int userNumber = 0;
+
+                for (int i = NUMBER_OF_GUESSES; i >= 0 && (userNumber != randomNumber); i--)//Countdown to number of guesses, it will iterate as long as the guesses remain above 
+                                                                                            //or is equa to one and as long as userNumber is not equal to randomNumber
                 {
-                    Console.WriteLine($"The number you have entered is too high, please try again.");
-                }
 
-                if (userNumber < randomNumber)
-                {
-                    Console.WriteLine("The number you have entered is too low, please try again.");
-                }
+                    userInput = Console.ReadLine();                               
+                    bool validInput = Int32.TryParse(userInput, out userNumber);
+
+                    while (userNumber > MAX_VALUE | userNumber < MIN_VALUE | !validInput)//For the input a number higher than maxValue
+                    {
+
+                        if (!validInput) //Incase, the user enters a letter after passing the initial scan for numerical input
+                        {
+                            Console.WriteLine("Please enter a numerical input:");
+                        }
+
+                        if (userNumber > MAX_VALUE)
+                        {
+                            Console.WriteLine($"Please Enter a number that is lower or equal to {MAX_VALUE}.");
+                        }
+
+                        if (userNumber < MIN_VALUE)
+                        {
+                            Console.WriteLine($"Please Enter a number that is higher than {MIN_VALUE}.");
+                        }
+
+                        userInput = Console.ReadLine();
+                        validInput = Int32.TryParse(userInput, out userNumber);
+
+                    }
+
                     Console.WriteLine("You have " + i + " tries left.");
-                    userNumber = Convert.ToInt32(Console.ReadLine());
 
-                if (i == 1)
-                {
-                    Console.WriteLine("Too many tries, better luck next time.");
+                    if ((userNumber >= (randomNumber - 5) && userNumber <= (randomNumber + 5)) && (userNumber != randomNumber))
+                    {
+                        Console.WriteLine("You're close!");
+                    }
+
+                    if (userNumber > randomNumber && userNumber <= MAX_VALUE && validInput)//Provides the code for when the user guesses a number that is higher than what the random number actually is
+                    {
+                        Console.WriteLine($"The number you have entered is too high, please try again.");
+                    }
+
+                    if (userNumber < randomNumber && userNumber >= MIN_VALUE && validInput)
+                    {
+                        Console.WriteLine("The number you have entered is too low, please try again.");
+                    }                   
+
                 }
-                
-                if (userNumber == randomNumber)
+
+                if (randomNumber == userNumber)
                 {
                     Console.WriteLine("You guessed it correctly! Congratulations!");
                 }
+
+                else
+                {
+                    Console.WriteLine("Too many tries, better luck next time.");
+                }
             }
-
-
         }
     }
 }
